3c7a3960912e39c51ef57ae60f149d12f4588f79
Arci <Arcidev@users.noreply.github.com>
2019-09-08 19:11:18 +0200

    Replaced Sql server for tests with in memory db as the first or default feature got fixed. Travis now also runs tests

--- src/BusinessLayer.Tests/TestBase.cs
+++ src/BusinessLayer.Tests/TestBase.cs
@@ -13,27 +13,13 @@ namespace BL.Tests
         static TestBase()
         {
             services = new ServiceCollection()
-                .AddDbContext<MusicLibraryDbContext>(options => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=MusicLibraryTests"), ServiceLifetime.Transient, ServiceLifetime.Transient)
+                .AddDbContext<MusicLibraryDbContext>(options => options.UseInMemoryDatabase("TestDB"), ServiceLifetime.Transient, ServiceLifetime.Transient)
                 .AddTransient<Func<DbContext>>(provider => () => provider.GetService<MusicLibraryDbContext>())
                 .ConfigureAutoMapper()
                 .ConfigureServices()
                 .ConfigureFacades()
                 .BuildServiceProvider();
 
-            InitTestDatabase();
-        }
-
-        private static void InitTestDatabase()
-        {
-            using var context = services.GetRequiredService<MusicLibraryDbContext>();
-            context.Database.EnsureCreated();
-
-            context.Categories.RemoveRange(context.Categories);
-            context.Bands.RemoveRange(context.Bands);
-            context.Songs.RemoveRange(context.Songs);
-            context.Users.RemoveRange(context.Users);
-
-            context.SaveChanges();
         }
     }
 }
